f8afce7bc2ae05cd7789ab4b9d4c3dbad3a2ede9
GumpGaming <anxin4392@gmail.com>
2024-06-27 10:15:53 -0400

    stripe

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,36 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const stripe = require('stripe')(functions.config().stripe.secret);
+const cors = require('cors')({ origin: true });
 
 admin.initializeApp();
 
-exports.createCheckoutSession = functions.https.onCall(async (data, context) => {
-    const { amount, success_url, cancel_url } = data;
+exports.createCheckoutSession = functions.https.onRequest((req, res) => {
+    cors(req, res, async () => {
+        const { amount, success_url, cancel_url } = req.body;
 
-    try {
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ['card'],
-            line_items: [{
-                price_data: {
-                    currency: 'usd',
-                    product_data: {
-                        name: 'Bike Purchase',
+        try {
+            const session = await stripe.checkout.sessions.create({
+                payment_method_types: ['card'],
+                line_items: [{
+                    price_data: {
+                        currency: 'usd',
+                        product_data: {
+                            name: 'Bike Purchase',
+                        },
+                        unit_amount: amount,
                     },
-                    unit_amount: amount,
-                },
-                quantity: 1,
-            }],
-            mode: 'payment',
-            success_url: success_url,
-            cancel_url: cancel_url,
-        });
+                    quantity: 1,
+                }],
+                mode: 'payment',
+                success_url: success_url,
+                cancel_url: cancel_url,
+            });
 
-        return { url: session.url };
-    } catch (error) {
-        console.error('Error creating Stripe Checkout session', error);
-        throw new functions.https.HttpsError('internal', 'Unable to create Stripe Checkout session');
-    }
+            res.json({ url: session.url });
+        } catch (error) {
+            console.error('Error creating Stripe Checkout session', error);
+            res.status(500).send({ error: 'Unable to create Stripe Checkout session' });
+        }
+    });
 });
